b41248a6b5695caa6d70edcb2a1f18cd10daacf9
Kieran Gee <kieran.lost.the.game@gmail.com>
2024-07-04 13:46:52 +0800

    more test

--- tests/test_launch.py
+++ tests/test_launch.py
@@ -1,22 +1,16 @@
 """Test launching the app."""
 
-import json
+from flask.testing import FlaskClient
+
 
 from mycoolapp import create_app
 
-# def test_config():
-#     assert not create_app().testing
-#     assert create_app({"TESTING": True}).testing
+def test_config():
+    assert not create_app().testing
+    assert create_app({"TESTING": True}).testing
 
 
-def test_hello(client):
+def test_hello(client: FlaskClient) -> None:
+    """This is a test I wrote."""
     response = client.get("/hello/")
-    # response_dict = response.json
-    # print("AAAAAAAAAAAAAAAAAAA")
-    # print(data)
-    # print("AAAAAAAAAAAAAAAAAAA")
-
-    print(response.json)
-    print(type(response.json))
-
-    assert response.json['msg'] == "Hello, World!"
+    assert response.json["msg"] == "Hello, World!"
