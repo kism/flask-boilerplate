cdb81c5851894cb618e1b1cfe7a25eff7a717b3b
Kieran Gee <kieran.lost.the.game@gmail.com>
2024-07-08 11:23:47 +0800

    fix

--- tests/test_launch.py
+++ tests/test_launch.py
@@ -34,6 +34,3 @@ def test_hello(client: FlaskClient) -> None:
     """Test the hello API endpoint."""
     response = client.get("/hello/")
     assert response.json["msg"] == "Hello, World!"
-
-
-test_config()
