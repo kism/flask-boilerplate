0521563ea0a513ef268f0726f59796935f4aee47
Kieran Gee <kieran.lost.the.game@gmail.com>
2024-07-15 13:48:16 +0800

    better tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,9 @@
 """Test launching the app and config."""
 
+import contextlib
 import logging
+import os
+import shutil
 
 import pytest
 
@@ -40,3 +43,18 @@ def test_config_file_creation(get_test_config: dict, caplog: pytest.LogCaptureFi
     caplog.set_level(logging.WARNING)
     create_app(test_config=get_test_config("testing_true_valid"), instance_path=pytest.TEST_INSTANCE_PATH)
     assert "No configuration file found, creating at default location:" not in caplog.text
+
+
+def test_config_file_loading(caplog: pytest.LogCaptureFixture):
+    """Tests relating to config file."""
+    shutil.copy(os.path.join(pytest.TEST_CONFIGS_LOCATION, "testing_true_valid.toml"), pytest.TEST_CONFIG_FILE_PATH)
+
+    # TEST: that file is created when no config is provided.
+    caplog.set_level(logging.INFO)
+    create_app(test_config=None, instance_path=pytest.TEST_INSTANCE_PATH)
+    assert "Using this path as it's the first one that was found" in caplog.text
+    caplog.clear()
+
+    # Remove any created config/logs
+    with contextlib.suppress(FileNotFoundError):
+        os.unlink(pytest.TEST_CONFIG_FILE_PATH)
