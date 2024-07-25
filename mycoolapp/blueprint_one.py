f10f6dfcc04166774ce245a7a62eb35194528506
Kieran Gee <kieran.lost.the.game@gmail.com>
2024-07-25 13:28:01 +0800

    demo object

--- mycoolapp/blueprint_one.py
+++ mycoolapp/blueprint_one.py
@@ -1,8 +1,10 @@
-"""Blueprints for..."""
+"""Blueprint one's object..."""
 
 import logging
 
 from flask import Blueprint, Response, current_app, jsonify
+
+from . import blueprint_one_object
 
 # Modules should all setup logging like this so the log messages include the modules name.
 # If you were to list all loggers with something like...
@@ -13,9 +15,9 @@ logger = logging.getLogger(__name__)  # Create a logger: mycoolapp.this_module_n
 # Register this module (__name__) as available to the blueprints of mycoolapp, I think https://flask.palletsprojects.com/en/3.0.x/blueprints/
 bp = Blueprint("mycoolapp", __name__)
 
-my_message = ""
+my_cool_object = None
 
-
+# KISM-BOILERPLATE:
 # So regarding current_app, have a read of https://flask.palletsprojects.com/en/3.0.x/appcontext/
 # This function is a bit of a silly example, but often you need to do things to initialise the module.
 # You can't use the current_app object outside of a function since it behaves a bit weird, even if
@@ -24,17 +26,36 @@ my_message = ""
 # You don't need to use this to set every variable as current_app will work fine in any function.
 def start_blueprint_one() -> None:
     """Method to 'configure' this module. Needs to be called under `with app.app_context():` from __init__.py."""
-    global my_message  # noqa: PLW0603 Necessary evil as far as I can tell, could move to all objects but eh...
-    my_message = current_app.config["app"]["my_message"]  # We set the my_message var globally
+    global my_cool_object  # noqa: PLW0603 Necessary evil as far as I can tell, could move to all objects but eh...
+    my_cool_object = blueprint_one_object.MyCoolObject(current_app.config)
 
 
 # KISM-BOILERPLATE: This is the demo api endpoint, enough to show a basic javascript interaction.
 @bp.route("/hello/", methods=["GET"])
 def get_hello() -> Response:
     """Hello GET Method."""
-    message = {"msg": my_message}
+    message = {"msg": current_app.config["app"]["my_message"]}
     status = 200
 
-    logger.debug("GET request to /hello/, returning: %s", current_app.config["app"])
+    logger.debug(
+        "GET request to /hello/, returning: %s as json, due to config: %s",
+        message,
+        current_app.config["app"],
+    )
+
+    return jsonify(message), status  # Return json, not a webpage.
+
+
+# KISM-BOILERPLATE: This is the demo api endpoint, enough to demonstrate object loading.
+@bp.route("/hello_backwards/", methods=["GET"])
+def get_hello_backwards() -> Response:
+    """Hello GET Method."""
+    message = {"msg": my_cool_object.get_my_message_backwards()}
+    status = 200
+
+    logger.debug(
+        "GET request to /hello_backwards/, returning: %s",
+        message,
+    )
 
     return jsonify(message), status  # Return json, not a webpage.
--- tests/test_blueprint_one.py
+++ tests/test_blueprint_one.py
@@ -1,6 +1,7 @@
 """Tests the blueprint's HTTP endpoint."""
 
 import logging
+from http import HTTPStatus
 
 from flask.testing import FlaskClient
 
@@ -10,6 +11,7 @@ from mycoolapp import create_app
 def test_hello(client: FlaskClient):
     """TEST: The default /hello/ response, This one uses the fixture in conftest.py."""
     response = client.get("/hello/")
+    assert response.status_code == HTTPStatus.OK
     assert response.json["msg"] == "Hello, World!", "Incorrect response from /hello/ when using default config."
 
 
@@ -17,10 +19,22 @@ def test_hello_with_config(tmp_path, get_test_config, caplog):
     """TEST: the hello API endpoint with non-default config."""
     app = create_app(get_test_config("bp_one_different_my_message.toml"), instance_path=tmp_path)
     client = app.test_client()
-    with caplog.at_level(logging.DEBUG):
-        response = client.get("/hello/")
-        assert (
-            response.json["msg"] == "Hello, PyTest!"
-        ), "Wrong response from /hello/ when loading custom message from config."
+    response = client.get("/hello/")
+    assert response.status_code == HTTPStatus.OK
+    assert response.json["msg"] == "Hello, PyTest!", "Incorrect response from /hello/ when using non-default config."
 
-    assert "GET request to /hello/, returning: {'my_message': 'Hello, PyTest!'}" in caplog.text
+    expected_log = (
+        "GET request to /hello/, returning: {'msg': 'Hello, PyTest!'} as json,"
+        " due to config: {'my_message': 'Hello, PyTest!'}"
+    )
+    with caplog.at_level(logging.DEBUG):
+        assert expected_log in caplog.text
+
+
+def test_hello_backwards(client: FlaskClient):
+    """TEST: The default /hello/ response, This one uses the fixture in conftest.py."""
+    response = client.get("/hello_backwards/")
+    assert response.status_code == HTTPStatus.OK
+    assert (
+        response.json["msg"] == "!dlroW ,olleH"  # cspell:disable-line
+    ), "Incorrect response from /hello_backwards/ when using default config."
